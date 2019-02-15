235fbded8b72c953f6b9cd125f7a12297a393b6d
Samuel Moncarey <samuel.moncarey@gmail.com>
2019-02-16 00:13:23 +0100

    Integrate node modules and styles with foundation-bundle

--- src/ContentWidgetsBundle.php
+++ src/ContentWidgetsBundle.php
@@ -2,8 +2,17 @@
 
 namespace MoncareyWS\ContentWidgetsBundle;
 
+use MoncareyWS\FoundationBundle\Bundle\BundleHasAssetsToBuild;
+use MoncareyWS\FoundationBundle\Bundle\BundleHasNodeModules;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ContentWidgetsBundle extends Bundle
+class ContentWidgetsBundle extends Bundle implements BundleHasNodeModules, BundleHasAssetsToBuild
 {
+    public function getGulpSassPaths(): array
+    {
+        return [
+            'src/scss',
+            'node_modules/trumbowyg/dist/ui/sass'
+        ];
+    }
 }
