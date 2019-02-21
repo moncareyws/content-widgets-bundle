3ceb26c3d2b5f17cc04192a3ca167d6f6e0dc6b6
Samuel Moncarey <samuel.moncarey@gmail.com>
2019-02-21 15:11:10 +0100

    Styling

--- src/ContentWidgetsBundle.php
+++ src/ContentWidgetsBundle.php
@@ -8,6 +8,10 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ContentWidgetsBundle extends Bundle implements BundleHasNodeModules, BundleHasAssetsToBuild
 {
+
+    /**
+     * @return array
+     */
     public function getGulpSassPaths(): array
     {
         return [
@@ -15,4 +19,24 @@ class ContentWidgetsBundle extends Bundle implements BundleHasNodeModules, Bundl
             'node_modules/trumbowyg/dist/ui/sass'
         ];
     }
+
+    /**
+     * @return array
+     */
+    public function getSassImports(): array
+    {
+        return [
+            'trumbowyg',
+            'content-widgets'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getSassIncludes(): array
+    {
+        return [];
+    }
+
 }
